12e32e0f4f674c1f52811a9fc043513fbb888676
Daniel_S <olgierd.dan@gmail.com>
2021-02-20 19:53:02 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'ab5db17a86d078c41b775921913af5e6'),
+    },
+  },
+});
