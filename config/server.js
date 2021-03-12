9dbb34a4eec13361274f6740827943883c495e41
Daniel_S <olgierd.dan@gmail.com>
2021-03-12 15:49:36 +0100

    Update project config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,9 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', 'ab5db17a86d078c41b775921913af5e6'),
+      secret: env("ADMIN_JWT_SECRET"),
     },
   },
 });
